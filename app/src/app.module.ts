6452037fd86fcd3c5ff315b589738aed163eb853
Sami Malik <sami.malik@appnovation.com>
2022-06-22 01:56:57 -0400

    Added the react app docker container.

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GlobalService } from './global/global.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GlobalService],
 })
 export class AppModule {}
---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Share data such as links, text, URL's, files and photos quickly between devices..
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ONEBOX
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
